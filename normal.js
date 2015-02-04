5ddce62d450d6a4597a80794adfd177736cc9cff
Katherine Trammell <ktrammell@gmail.com>
2015-02-04 17:12:57 -0500

    updated normal with answers to 6-8

--- normal.js
+++ normal.js
@@ -55,12 +55,12 @@ function square(number) {
 // and return it.
 
 function sum (numbers) {
-  x = 0;
-  for (var index = 0; index < numbers.length; index++) {
-  x = x + numbers[index];
-  }
-    return(x);
-  }
+   var total = 0;
+   for (var index = 0; index < numbers.length; index++) {
+      total = total + numbers[index];
+   }
+   return total;
+}
 
 //---------
 
@@ -73,10 +73,47 @@ function sum (numbers) {
 
 // HINT: use an array and a for loop
 
-function letterIndex(x) {
-  for (var index = 1; index < x.length; index++) {
-  console.log(index);
+// alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+
+function letterIndex(letter) {
+  var lowerCase, alphabet, index; 
+  var alphabet = { //this is an object, set a key and value
+    "a" : 0,
+    "b" : 1,
+    "c" : 2,
+    "d" : 3,
+    "e" : 4,
+    "f" : 5,
+    "g" : 6,
+    "h" : 7,
+    "i" : 8,
+    "j" : 9,
+    "k" : 10,
+    "l" : 11,
+    "m" : 12,
+    "n" : 13,
+    "o" : 14,
+    "p" : 15,
+    "q" : 16,
+    "r" : 17,
+    "s" : 18,
+    "t" : 19,
+    "u" : 20,
+    "v" : 21,
+    "w" : 22,
+    "x" : 23,
+    "y" : 24,
+    "z" : 25,
+  };
+
+  lowerCase = letter.toLowerCase(); //this changes data entered in letter to lower case
+  index = alphabet[lowerCase]; //this give the index a value that can be returned
+  
+  return index;
 }
+
+
 
 //---------
 
@@ -92,8 +129,15 @@ function letterIndex(x) {
 
 // HINT: use an array and the modulo (%) operator
 
-function reverseLetterIndex() {
+function reverseLetterIndex(index) {
+  var letter, alphabet = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", 
+  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
+  "w", "x", "y", "z"];
 
+  letter = alphabet[index % alphabet.length];
+  //reassigns remainder to the index to allow loop
+
+  return letter;
 }
 
 //---------
@@ -108,6 +152,52 @@ function reverseLetterIndex() {
 
 // HINT: use the previous 2 functions
 
-function rot13() {
+function rot13(sentence) {
+  var cipheredCode;
 
+  function codeLetter (letter) {
+    var index = letterIndex(letter);  //this pulls letterIndex from #5 question
+    var indexShifted = index + 13;
+    var letterShifted = reverseLetterIndex(indexShifted); //this pulls reverseLetterIndex from #6
+    return letterShifted;
+  }
+
+  function codeWord (word) {
+    var letters = word.split(""); //this splits the letters of the word
+    var combinedLetters = []; //this says that the letters are in an array
+    var processedLetter, letter;
+    
+    for (var index = 0; index < letters.length; index ++) {
+      letter = letters[index];
+      processedLetter = codeLetter(letter); //this pulls in codeLetter from function above
+      combinedLetters.push(processedLetter); //this takes the individual letters out of the array and turns into a script    
+    }
+    var processedWord = combinedLetters.join(""); //this joins the letters back together to create the word
+    return processedWord;
+  }
+
+    var words = sentence.split(" "); // this splits each word at the space mark
+    var word, cipheredWord, finishedWords = [];
+
+    for (var index = 0; index < words.length; index ++) {
+      word = words[index];
+      cipheredWord = codeWord(word); //this pulls in the processed work from function above
+      finishedWords.push(cipheredWord); // this takes the ciphered words out of the array and puts them into string
+    }
+    cipheredCode = finishedWords.join(" "); //this puts the string back in place with space marks
+
+
+    return cipheredCode;
+}
+
+
+
+
+
+
+
+
+
+
+return cipheredCode
 }
